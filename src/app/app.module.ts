d5a3c7543b53ddfed8481c9156bd04b52cea4811
Inara Oenning <inaraoenning@gmail.com>
2025-08-08 15:49:31 -0300

    update: @angular/core para @angular/platform-browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
